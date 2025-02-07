165f672638d8ce2b6e96b284a1eba6b9f0102d85
chingliu10 <56655368+chingliu10@users.noreply.github.com>
2025-02-07 19:42:08 +0300

    we created the view all incomes

--- controllers/incomeController.js
+++ controllers/incomeController.js
@@ -4,7 +4,7 @@ import handleError from '../helpers/handleError.js';
 const { pool } = connection;
 
 // Get all payroll entries
-export const storeIncomeType = async (income_type) => {
+export const storeIncomeType = async (income_type, branch ) => {
 
 
     try {
@@ -12,11 +12,11 @@ export const storeIncomeType = async (income_type) => {
         let timeStamp = new Date()
 
         const query = `
-           INSERT INTO other_income_types
-            (name, created_at, updated_at) values (?, ?, ?)
+           INSERT INTO other_income_types 
+            (name, created_at, updated_at, branch_id) values (?, ?, ?, ?)
         `;
 
-        await pool.query(query, [income_type.toUpperCase(), timeStamp , timeStamp ])
+        await pool.query(query, [income_type.toUpperCase(), timeStamp , timeStamp, branch ])
 
         return { queryStatus: true }
 
@@ -27,3 +27,85 @@ export const storeIncomeType = async (income_type) => {
     }
 
 };
+
+
+export const getAllIncomeTypes = async ( branch ) => {
+
+    try {
+
+        const query = `
+            SELECT id, name FROM other_income_types where branch_id = ?
+        `
+
+        let [rows] = await pool.query(query, [ branch ])
+
+        return {
+            queryStatus : true,
+            data : rows
+        }
+
+    }catch (error) {
+
+        return handleError(error, "Failed To Get Income Types/c")
+
+    }
+
+}
+
+
+export const storeIncome = async ( {  income_type , amount, income_date, notes } , branch ) => {
+
+    try {
+
+        let timeStamp = new Date()
+
+        let query = `
+            insert into other_income (other_income_type_id, date, amount, notes, created_at, updated_at, branch_id) 
+                values (?, ?, ?, ?, ? , ?, ?)
+        `
+
+        await pool.query(query, [income_type , income_date, amount, notes, timeStamp, timeStamp, branch])
+
+        return {
+            queryStatus : true
+        }
+
+    }catch (error) {
+
+        console.log(error)
+        handleError(error , "Failed To Add Income/c")
+
+    }
+
+}
+
+
+export const getAllIncomes = async (branch) => {
+
+
+    try {
+
+        let query = `
+            select other_income.other_income_type_id as 
+	income_id  , other_income.amount as amount , 
+		other_income.date as date , other_income.notes as description ,
+		  other_income_types.name as income_type	from other_income join other_income_types on other_income.other_income_type_id = 
+	other_income_types.id where  other_income.branch_id = ? and  other_income.deleted_at is null
+		and other_income_types.deleted_at is null;
+        `
+
+        let [rows] = await pool.query(query, [branch])
+
+        console.log(rows)
+
+        return {
+            queryStatus : true,
+            data : rows
+        }
+
+    }catch (error) {
+
+        handleError(error , "Failed To Get Incomes/c")
+
+    }
+}
--- routes/income.js
+++ routes/income.js
@@ -1,25 +1,111 @@
 import express from 'express';
 import {
-    storeIncomeType
+    getAllIncomeTypes,
+    storeIncomeType,
+    storeIncome,
+    getAllIncomes
 } from '../controllers/incomeController.js';
 import handleError from '../helpers/handleError.js';
+import { isAuthenticated } from '../middlewares/isAuthenticated.js';
 
 const router = express.Router();
 
 router.use(express.json());
+router.use(isAuthenticated)
 
 //all incomes
-router.get("/view", async (req, res) => {
-    res.send("all incomes")
+router.get("/all", async (req, res) => {
+    try {
+
+        let result = await getAllIncomes(req.session.user.branchId)
+
+        if(result.queryStatus) {
+
+            return res.render("income_list", { title : "All Income" , user : req.session.user , incomes : result.data })
+
+        }
+
+        res.status(400).render("error_page", { message : result.activity })
+
+    }catch (error) {
+
+        res.status(500).render("error_page", { message : "Failed To Fetch Income Add Route/r" })
+
+    }
+})
+
+//add income 
+router.get("/add", async (req, res) => {
+    try {
+
+        let result = await getAllIncomeTypes(req.session.user.branchId)
+
+        if(result.queryStatus) {
+
+            return res.render("income_add", { title : "Add Income" , user : req.session.user , incomeTypes : result.data })
+
+        }
+
+        res.status(400).render("error_page", { message : result.activity })
+
+    }catch (error) {
+
+        res.status(500).render("error_page", { message : "Failed To Fetch Income Add Route/r" })
+
+    }
+})
+
+//storeIncome
+router.post("/add", async (req, res) => {
+
+    
+    try {
+
+        let result = await storeIncome(req.body, req.session.user.branchId)
+
+        if(result.queryStatus) {
+
+            req.flash("success", "Income Added Successfully")
+            return res.redirect(`/income/all`)
+
+        }
+
+        res.status(400).render("error_page", { message : result.activity })
+
+    }catch (error) {
+
+        res.status(500).render("error_page", { message : "Failed To Add Income/r" })
+
+    }
+
 })
 
 //view all income types
 router.get("/type/all", async (req, res) => {
-    res.render("income_type_list")
+
+    try {
+
+        let result = await getAllIncomeTypes(req.session.user.branchId)
+
+        if(result.queryStatus) {
+
+            return res.render("income_type_list", { title : "All Income Types" , user : req.session.user , incomeTypes : result.data })
+
+        }
+
+        res.status(400).render("error_page", { message : result.activity })
+
+
+    }catch (error) {
+
+        res.status(500).render("error_page", { message : "Failed To Get Income Types/r" })
+
+    }
+    
 })
 
 router.get("/type/add", async (req, res) => {
-    res.render("income_type_add")
+    res.render("income_type_add", { title : "Add Income Type" , user : req.session.user })
 })
 
 router.post("/type/add", async (req, res) => {
@@ -28,7 +114,7 @@ router.post("/type/add", async (req, res) => {
 
         let { income_type_add } = req.body
 
-        let result = await storeIncomeType(income_type_add)
+        let result = await storeIncomeType(income_type_add, req.session.user.branchId )
 
         if(result.queryStatus) {
             
